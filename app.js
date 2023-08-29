92061e43ecbbed2a12545fb1df0980b531ba4a38
Elu1919 <qw29149974@gmail.com>
2023-08-29 23:40:26 +0800

    feat: add handlebars & fix app.js

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ if (process.env.NODE.ENV !== 'production') {
 
 // require
 const express = require('express')
-const router = require('./routes')
+const handlebars = require('express-handlebars')
+
+const { pages } = require('./routes')
 
 // setting
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(router)
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(pages)
 
 // listening on app
 app.listen(port, () => {
