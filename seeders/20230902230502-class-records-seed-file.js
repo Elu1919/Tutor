5edd284e72a472827f749cc1068b56fd2676011d
Elu1919 <qw29149974@gmail.com>
2023-09-08 01:08:35 +0800

    feat: add class-record seeder

--- seeders/20230902230502-class-records-seed-file.js
+++ seeders/20230902230502-class-records-seed-file.js
@@ -12,10 +12,36 @@ module.exports = {
     for (let i = 0; i < 50; i++) {
       const h = Math.floor(Math.random() * 4 + 18)
       const d = Math.floor(Math.random() * 30 + 1)
-      const mon = Math.floor(Math.random() * 9 + 1)
+      const mon = Math.floor(Math.random() * 8 + 1)
 
       const teacherId = i < 25 ? users[(i + 1) * 2 - 2].id : users[(i - 24) * 2 - 2].id
       const studentId = i < 25 ? users[i * 2 + 1].id : users[(i - 25) * 2 + 1].id
+      const startTime = new Date(2023, mon, d, h)
+      const endTime = new Date(2023, mon, d, h, 30)
+      const score = Math.floor(Math.random() * 11)
+      const comment = faker.person.jobTitle()
+
+      await queryInterface.bulkInsert('ClassRecords', [
+        {
+          teacher_id: teacherId,
+          student_id: studentId,
+          start_time: startTime,
+          end_time: endTime,
+          score: score,
+          comment: comment,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    }
+    // 老師 已完成 已評分 各2個
+    for (let i = 0; i < 25; i++) {
+      const h = Math.floor(Math.random() * 4 + 18)
+      const d = Math.floor(Math.random() * 30 + 1)
+      const mon = Math.floor(Math.random() * 8 + 1)
+
+      const teacherId = users[(i + 1) * 2 - 2].id
+      const studentId = i < 24 ? users[(i + 2) * 2 - 2].id : users[0].id
       const startTime = new Date(2023, mon, d, h)
       const endTime = new Date(2023, mon, d, h, 30)
       const score = Math.floor(Math.random() * 11)
@@ -38,10 +64,36 @@ module.exports = {
     for (let i = 0; i < 50; i++) {
       const h = Math.floor(Math.random() * 4 + 18)
       const d = Math.floor(Math.random() * 30 + 1)
-      const mon = Math.floor(Math.random() * 9 + 1)
+      const mon = Math.floor(Math.random() * 8 + 1)
 
       const teacherId = i < 25 ? users[(i + 1) * 2 - 2].id : users[(i - 24) * 2 - 2].id
       const studentId = i < 25 ? users[i * 2 + 1].id : users[(i - 25) * 2 + 1].id
+      const startTime = new Date(2023, mon, d, h)
+      const endTime = new Date(2023, mon, d, h, 30)
+      const score = 0
+      const comment = ''
+
+      await queryInterface.bulkInsert('ClassRecords', [
+        {
+          teacher_id: teacherId,
+          student_id: studentId,
+          start_time: startTime,
+          end_time: endTime,
+          score: score,
+          comment: comment,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    }
+    // 老師 已完成 未評分 各2個
+    for (let i = 0; i < 25; i++) {
+      const h = Math.floor(Math.random() * 4 + 18)
+      const d = Math.floor(Math.random() * 30 + 1)
+      const mon = Math.floor(Math.random() * 8 + 1)
+
+      const teacherId = users[(i + 1) * 2 - 2].id
+      const studentId = i < 24 ? users[(i + 2) * 2 - 2].id : users[0].id
       const startTime = new Date(2023, mon, d, h)
       const endTime = new Date(2023, mon, d, h, 30)
       const score = 0
@@ -68,6 +120,32 @@ module.exports = {
 
       const teacherId = i < 25 ? users[(i + 1) * 2 - 2].id : users[(i - 24) * 2 - 2].id
       const studentId = i < 25 ? users[i * 2 + 1].id : users[(i - 25) * 2 + 1].id
+      const startTime = new Date(2023, mon, d, h)
+      const endTime = new Date(2023, mon, d, h, 30)
+      const score = 0
+      const comment = ''
+
+      await queryInterface.bulkInsert('ClassRecords', [
+        {
+          teacher_id: teacherId,
+          student_id: studentId,
+          start_time: startTime,
+          end_time: endTime,
+          score: score,
+          comment: comment,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    }
+    // 老師 預約 未上課 各2個
+    for (let i = 0; i < 25; i++) {
+      const h = Math.floor(Math.random() * 4 + 18)
+      const d = Math.floor(Math.random() * 14 + 15)
+      const mon = 9
+
+      const teacherId = users[(i + 1) * 2 - 2].id
+      const studentId = i < 24 ? users[(i + 2) * 2 - 2].id : users[0].id
       const startTime = new Date(2023, mon, d, h)
       const endTime = new Date(2023, mon, d, h, 30)
       const score = 0
--- test.js
+++ test.js
@@ -3,45 +3,7 @@ const bcrypt = require('bcryptjs')
 const { faker } = require('@faker-js/faker')
 const { Lesson } = require('./models')
 
-const test = async () => {
-  const hash = await bcrypt.hash('12345678', 10)
-  const week = [1, 2, 3, 4, 5, 6, 7]
-  console.log(
-    await Array.from({ length: 30 }, (
-      lesson,
-      id,
-      teacherId = id * 2 + 1,
-      time = Math.floor(Math.random() * 2) !== 0 ? 30 : 60,
-      date = Math.floor(Math.random() * 5),
-      totalScore = (id + 1) % 2 === 0 ? 0 : 1,
-      scoreCount = 0) => (
-      {
-        // teacher_id: teacherId,
-        name: faker.person.jobTitle(),
-        info: faker.lorem.text(),
-        style: faker.lorem.text(),
-        // link: 'https://meet.google.com/?sjid=663613846954092567-AP',
-        // time: time,
-        // date: `${week[date]}${week[date + 1]}${week[date + 2]}`,
-        // img: 'https://upload.cc/i1/2023/08/27/bex9jv.png',
-        // total_score: totalScore,
-        // score_count: scoreCount,
-        // created_at: new Date(),
-        // updated_at: new Date()
-      }))
-  )
-}
-
-const date = async () => {
-  const data = await Lesson.findByPk(1, {
-    raw: true
-  })
-  console.log(data.createdAt)
-  console.log(new Date(data.createdAt))
-  console.log(moment(new Date()).format('YYYY-MM-DD HH:mm:hh'))
-}
-
-const date02 = () => {
+const date = () => {
   for (let i = 0; i < 10; i++) {
     ConsoleRandomDate();
   }
@@ -70,4 +32,9 @@ function RandomDate() {
 }
 
 // console.log(RandomDate())
-date02()
+
+for (let i = 0; i < 25; i++) {
+  const teacherId = (i + 1) * 2 - 2
+  const studentId = i < 24 ? (i + 2) * 2 - 2 : 0
+  console.log(teacherId, studentId)
+}
