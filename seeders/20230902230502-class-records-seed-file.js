344a206169feb05870ebc1802302a78d75bfbc41
Elu1919 <qw29149974@gmail.com>
2023-09-08 00:51:52 +0800

    feat: add class-record seeder

--- seeders/20230902230502-class-records-seed-file.js
+++ seeders/20230902230502-class-records-seed-file.js
@@ -4,34 +4,92 @@ const { faker } = require('@faker-js/faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const week = [1, 2, 3, 4, 5, 6, 7]
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
-    await queryInterface.bulkInsert('ClassRecords',
-      Array.from({ length: 50 }, (
-        record,
-        id,
-        teacherId = id < 25 ? users[id * 2 + 1].id : users[id * 2 + 1].id,
-        studentId = users[id],
-        startTime = Math.floor(Math.random() * 2) !== 0 ? 30 : 60,
-        endTime = Math.floor(Math.random() * 5),
-        isCommented = Math.floor(Math.random() * 2)) => (
+    // 學生 已完成 已評分 各2個 
+    for (let i = 0; i < 50; i++) {
+      const h = Math.floor(Math.random() * 4 + 18)
+      const d = Math.floor(Math.random() * 30 + 1)
+      const mon = Math.floor(Math.random() * 9 + 1)
+
+      const teacherId = i < 25 ? users[(i + 1) * 2 - 2].id : users[(i - 24) * 2 - 2].id
+      const studentId = i < 25 ? users[i * 2 + 1].id : users[(i - 25) * 2 + 1].id
+      const startTime = new Date(2023, mon, d, h)
+      const endTime = new Date(2023, mon, d, h, 30)
+      const score = Math.floor(Math.random() * 11)
+      const comment = faker.person.jobTitle()
+
+      await queryInterface.bulkInsert('ClassRecords', [
         {
           teacher_id: teacherId,
           student_id: studentId,
           start_time: startTime,
           end_time: endTime,
-          score: isCommented !== 0 ? '' : Math.floor(Math.random() * 11),
-          comment: isCommented !== 0 ? '' : faker.person.jobDescriptor(),
+          score: score,
+          comment: comment,
           created_at: new Date(),
           updated_at: new Date()
-        }))
-    )
+        }
+      ])
+    }
+    // 學生 已完成 未評分 各2個
+    for (let i = 0; i < 50; i++) {
+      const h = Math.floor(Math.random() * 4 + 18)
+      const d = Math.floor(Math.random() * 30 + 1)
+      const mon = Math.floor(Math.random() * 9 + 1)
+
+      const teacherId = i < 25 ? users[(i + 1) * 2 - 2].id : users[(i - 24) * 2 - 2].id
+      const studentId = i < 25 ? users[i * 2 + 1].id : users[(i - 25) * 2 + 1].id
+      const startTime = new Date(2023, mon, d, h)
+      const endTime = new Date(2023, mon, d, h, 30)
+      const score = 0
+      const comment = ''
+
+      await queryInterface.bulkInsert('ClassRecords', [
+        {
+          teacher_id: teacherId,
+          student_id: studentId,
+          start_time: startTime,
+          end_time: endTime,
+          score: score,
+          comment: comment,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    }
+    // 學生 預約 未上課 各2個
+    for (let i = 0; i < 50; i++) {
+      const h = Math.floor(Math.random() * 4 + 18)
+      const d = Math.floor(Math.random() * 14 + 15)
+      const mon = 9
+
+      const teacherId = i < 25 ? users[(i + 1) * 2 - 2].id : users[(i - 24) * 2 - 2].id
+      const studentId = i < 25 ? users[i * 2 + 1].id : users[(i - 25) * 2 + 1].id
+      const startTime = new Date(2023, mon, d, h)
+      const endTime = new Date(2023, mon, d, h, 30)
+      const score = 0
+      const comment = ''
+
+      await queryInterface.bulkInsert('ClassRecords', [
+        {
+          teacher_id: teacherId,
+          student_id: studentId,
+          start_time: startTime,
+          end_time: endTime,
+          score: score,
+          comment: comment,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    }
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('ClassRecords', {})
   }
 }
+
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+const moment = require('moment')
 const bcrypt = require('bcryptjs')
 const { faker } = require('@faker-js/faker')
+const { Lesson } = require('./models')
 
 const test = async () => {
   const hash = await bcrypt.hash('12345678', 10)
@@ -30,4 +32,42 @@ const test = async () => {
   )
 }
 
-test()
+const date = async () => {
+  const data = await Lesson.findByPk(1, {
+    raw: true
+  })
+  console.log(data.createdAt)
+  console.log(new Date(data.createdAt))
+  console.log(moment(new Date()).format('YYYY-MM-DD HH:mm:hh'))
+}
+
+const date02 = () => {
+  for (let i = 0; i < 10; i++) {
+    ConsoleRandomDate();
+  }
+  function ConsoleRandomDate() {
+    const h = Math.floor(Math.random() * 4 + 18)
+    const d = Math.floor(Math.random() * 14 + 15)
+    const m = Math.floor(Math.random() * 3 + 9)
+    const maxdaterandom = new Date().getTime();
+    const mindaterandom = new Date(2023, m, d, h, 30).getTime();
+    const randomdate = getRandom(mindaterandom, maxdaterandom);
+    const datestr = moment(mindaterandom).format("YYYY-MM-DD HH:mm:hh");
+    console.log(datestr);
+  }
+  function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
+function RandomDate() {
+  const h = Math.floor(Math.random() * 4 + 18)
+  const d = Math.floor(Math.random() * 30 + 1)
+  const m = Math.floor(Math.random() * 12 + 1)
+  return new Date(2023, m, d, h)
+}
+
+// console.log(RandomDate())
+date02()
