d50d71b71cec994a0292983351681df58987477b
Elu1919 <qw29149974@gmail.com>
2023-09-11 04:02:52 +0800

    feat: fix admin back

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
 const authenticated = (req, res, next) => {
   // if (req.isAuthenticated)
   if (ensureAuthenticated(req)) {
-    if (getUser(req).is_admin) return res.redirect('back')
+    if (getUser(req).is_admin) return res.redirect('/admin/users')
     return next()
   }
   res.redirect('/login')
