ceeed87f8f2b32f143b91a9781fd8a7b96dedd0c
Elu1919 <qw29149974@gmail.com>
2023-09-10 21:19:19 +0800

    feat: add api logout removed

--- services/admin-services.js
+++ services/admin-services.js
@@ -22,19 +22,6 @@ const adminServices = {
         })
       })
       .catch(err => cb(err))
-  },
-  loginPage: (req, res, next) => {
-    res.render('admin/login')
-  },
-  login: (req, res, next) => {
-    req.flash('success_messages', '成功登入！')
-    res.redirect('/admin/users')
-  },
-  logout: (req, res, next) => {
-    req.logout(err => {
-      if (err) return next(err)
-    })
-    res.redirect('/login/admin')
   }
 }
 
